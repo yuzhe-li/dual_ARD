042c9403cd3d2d12243b6a335964166a6cd10849
yuzhe-li <30654454+yuzhe-li@users.noreply.github.com>
2024-12-10 22:15:40 +0900

    Add files via upload

---
+++ functions/plt_rcparams.py
@@ -0,0 +1,21 @@
+
+import matplotlib.pyplot as plt
+scale = 5
+cm2inch = 0.39
+ssmall_SIZE = 3*scale
+SMALL_SIZE = 4*scale
+MEDIUM_SIZE = 6*scale
+BIGGER_SIZE = 8*scale
+LINE_WIDTH = 2*2
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+
+plt.rc('axes', labelsize=SMALL_SIZE)    # fontsize of the x and y labels
+plt.rc('figure', titlesize=MEDIUM_SIZE)  # fontsize of the figure title
+plt.rc('axes', titlesize=MEDIUM_SIZE)     # fontsize of the axes title
+plt.rc('lines', linewidth=LINE_WIDTH)
+
+
+
